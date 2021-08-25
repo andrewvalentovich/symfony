0892d213cf7556dd400a1d32184b92d2a8a93fee
Андрей Валентович <andrich_14@mail.ru>
2021-08-25 17:56:02 +0300

    Homework has been realised

--- Homeworks/Homework07/src/Controller/Admin/tagsController.php
+++ Homeworks/Homework07/src/Controller/Admin/tagsController.php
@@ -2,33 +2,32 @@
 
 namespace App\Controller\Admin;
 
-use App\Repository\CommentRepository;
+use App\Repository\TagRepository;
 use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class commentsController extends AbstractController
+class tagsController extends AbstractController
 {
     /**
-     * @Route("/admin/comments", name="app_admin_comments")
+     * @Route("/admin/tags", name="app_admin_tags")
      */
-
-    public function index(Request $request, CommentRepository $commentRepository, PaginatorInterface $paginator)
+    public function index(Request $request, TagRepository $tagRepository, PaginatorInterface $paginator): Response
     {
         $countView = ($request->query->get('countView') == 'noValue') ? 20 : $request->query->get('countView');
 
         $pagination = $paginator->paginate(
-            $commentRepository->findAllWithSearchAndSoftDelNoQuery(
+            $tagRepository->findAllWithSoftDelNoResult(
                 $request->query->get('q'),
                 $request->query->has('showDeleted')
             ), /* query NOT result */
             $request->query->getInt('page', 1), /*page number*/
-            $countView /*limit per page*/
+            $countView/*limit per page*/
         );
 
-
-        return $this->render('admin/comments/index.html.twig', [
+        return $this->render('admin/tags/index.html.twig', [
             'pagination' => $pagination,
         ]);
     }
