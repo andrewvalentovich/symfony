bb2e86810711668adc0074212702617315e0d5b5
Андрей Валентович <andrich_14@mail.ru>
2021-08-26 22:35:43 +0300

    Lecture 8.4 end

--- Homeworks/Lectures/src/DataFixtures/UserFixtures.php
+++ Homeworks/Lectures/src/DataFixtures/UserFixtures.php
@@ -5,16 +5,29 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends BaseFixtures
 {
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $userPasswordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
     public function loadData(ObjectManager $manager)
     {
         $this->createMany(User::class, 10, function (User $user) {
            $user
                ->setEmail($this->faker->email)
                ->setFirstName($this->faker->firstName)
-               ->setPassword('123')
+               ->setPassword($this->userPasswordEncoder->encodePassword($user, '123'))
            ;
         });
     }
---
+++ Homeworks/Lectures/src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use function Doctrine\Common\Cache\Psr6\get;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+    /**
+     * @var CsrfTokenManagerInterface
+     */
+    private $csrfTokenManager;
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $userPasswordEncoder;
+
+
+    public function __construct(
+        UserRepository $userRepository,
+        UrlGeneratorInterface $urlGenerator,
+        CsrfTokenManagerInterface $csrfTokenManager,
+        UserPasswordEncoderInterface $userPasswordEncoder
+    ) {
+        $this->userRepository = $userRepository;
+        $this->urlGenerator = $urlGenerator;
+        $this->csrfTokenManager = $csrfTokenManager;
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    protected function getLoginUrl()
+    {
+        return $this->urlGenerator->generate('app_login');
+    }
+
+    public function supports(Request $request)
+    {
+        return $request->attributes->get('_route') === 'app_login' && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        $credentials = [
+            'email'     => $request->request->get('email'),
+            'password'  => $request->request->get('password'),
+            '_csrf_toker' => $request->request->get('_csrf_toker'),
+        ];
+
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $credentials['email']
+        );
+
+
+
+        return $credentials;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        if ($credentials['email'] == 'gisselle57@feil.org') {
+            throw new CustomUserMessageAuthenticationException('Ухади');
+        }
+
+        $csrfToken = new CsrfToken('authenticate', $credentials['_csrf_toker']);
+
+        if (! $this->csrfTokenManager->isTokenValid($csrfToken)) {
+            throw new InvalidCsrfTokenException();
+        }
+
+        return $this->userRepository->findOneBy(['email' => $credentials['email']]);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return $this->userPasswordEncoder->isPasswordValid($user, $credentials['password']);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    {
+        return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
+    }
+
+}
