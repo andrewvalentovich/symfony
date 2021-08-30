914f51178198931a316271fca229edcabefec2bc
Андрей Валентович <andrich_14@mail.ru>
2021-08-30 15:21:46 +0300

    Lecture 9.2 has been studied

--- Homeworks/Lectures/src/Controller/AccountController.php
+++ Homeworks/Lectures/src/Controller/AccountController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ * @method User|null getUser()
+ */
 class AccountController extends AbstractController
 {
     /**
      * @Route("/account", name="app_account")
-     * @IsGranted("ROLE_USER")
      */
     public function index(): Response
     {
