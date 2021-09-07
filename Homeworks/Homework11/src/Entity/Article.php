f42ec56075aed7d72b88a428914260487d878613
Андрей Валентович <andrich_14@mail.ru>
2021-09-07 21:55:38 +0300

    Project has been modified

--- Homeworks/Homework11/src/Entity/Article.php
+++ Homeworks/Homework11/src/Entity/Article.php
@@ -42,11 +42,6 @@ class Article
     private $body;
 
     /**
-     * @ORM\Column(type="string", length=100)
-     */
-    private $author;
-
-    /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $keywords;
@@ -88,6 +83,12 @@ class Article
      * @ORM\ManyToMany(targetEntity=Tag::class, inversedBy="articles")
      */
     private $tags;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
 
     public function __construct()
     {
@@ -145,18 +146,6 @@ class Article
     public function setBody(string $body): self
     {
         $this->body = $body;
-
-        return $this;
-    }
-
-    public function getAuthor(): ?string
-    {
-        return $this->author;
-    }
-
-    public function setAuthor(string $author): self
-    {
-        $this->author = $author;
 
         return $this;
     }
@@ -297,6 +286,18 @@ class Article
     public function removeTag(Tag $tag): self
     {
         $this->tags->removeElement($tag);
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
