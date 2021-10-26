6ec9488e893bdde74bb186282f2285dd8488a35c
Андрей Валентович <andrich_14@mail.ru>
2021-10-26 16:55:36 +0300

    Register notification and Last Comments block has been realised

---
+++ Homeworks/Homework/src/EventSubscriber/UserRegisteredSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+
+use App\Events\UserRegisteredEvent;
+use App\Service\Mailer;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class UserRegisteredSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var Mailer
+     */
+    private $mailer;
+
+    public function __construct(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function onUserRegistered(UserRegisteredEvent $event)
+    {
+        $user = $event->getUser();
+
+        $this->mailer->sendMail(
+            $user->getEmail(),
+            $user->getFirstName(),
+            'Spill-Coffee-On-The-Keyboard',
+            'email/welcome.html.twig',
+            function (TemplatedEmail $email) use ($user){
+                $email
+                    ->context([
+                        'user'  =>  $user
+                    ])
+                ;
+            }
+        );
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            UserRegisteredEvent::class => 'onUserRegistered'
+        ];
+    }
+}
